995a08ffa46678e30e2f1cb0b5b8f0fe21b3ac31
Irem Sarihan <iremsarihan@gmail.com>
2020-06-02 12:44:05 -0400

    Update Pre_QC.sh

--- Pre_QC.sh
+++ Pre_QC.sh
@@ -48,3 +48,17 @@ plink --bfile For_PCA_maf5_geno2_mind5_hwe_MHCInverfree --exclude Random300_LDpr
 
 ##  PCA
 plink --bfile For_PCA_maf5_geno2_mind5_hwe_MHCInverfree_LDpruned --pca header tabs --keep-allele-order --out data_pca
+
+
+## Admixture run
+for K in 1 2 3 4 5 6 7 8 9 10; do admixture --cv For_PCA_maf5_geno2_mind5_hwe_MHCInverfree_LDpruned.bed $K -j10 | tee log${K}.out; done
+
+
+## For relatedness estimation with KING
+# Get a list of unrelated individuals
+king -b For_PCA_maf5_geno2_mind5_hwe.bed --unrelated --degree 2
+
+# Get pairwise relatedness degree estimations
+king -b For_PCA_maf5_geno2_mind5_hwe.bed --related  --degree 2 --rplot --prefix king_output
+
+

83dd8d12b2316f3f187c9dfd53a1c6c42c391ae2
Irem Sarihan <iremsarihan@gmail.com>
2020-06-02 12:52:36 -0400

    Create PennCNV.sh

---
+++ PennCNV.sh
@@ -0,0 +1,2 @@
+# Create intensity files from the initial Illumina report
+perl PennCNV/1.0.4/split_illumina_report.pl -prefix PD_CNV/data/intensity_files/ PD_CNV/data/mata_grc_mega_1_all_180305_FinalReport.txt 

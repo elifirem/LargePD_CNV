7e7a6decb245b99f57790f57489120593bb19b10
Irem Sarihan <iremsarihan@gmail.com>
2020-06-02 13:44:47 -0400

    Update Post_QC.sh

--- Post_QC.sh
+++ Post_QC.sh
@@ -1,6 +1,9 @@
 ## Set lrrsd threshold so stringent that no sample passes(to get summary stats): 
 perl PennCNV/1.0.5/filter_cnv.pl Large_PD_conf.clean.rawcnv -qclogfile Large_PD_conf.log -qclrrsd 0.0001 -qcpassout Large_PD_conf.clean.qcpass -qcsumout Large_PD_conf.clean.qcsum -out Large_PD_conf.clean.goodcnv
 
+## In R:
+# Read in the .qcsum file
+# Plot the distribution of number of CNVs per sample in a histogram and decide the cutoff
 
 
 perl PennCNV/1.0.5/filter_cnv.pl Large_PD_conf.clean.rawcnv -qclogfile Large_PD_conf.log -qcnumcnv 1639 -qclrrsd 0.2720423 -qcwf 0.03110594 -qcbafdrift 0.001405433 -qcpassout Large_PD_conf2.clean.qcpass -qcsumout Large_PD_conf2.clean.qcsum -out Large_PD_conf2.clean.goodcnv
